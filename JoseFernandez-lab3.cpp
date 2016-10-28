67315d0cc181599435b23b08f11a1aa7eba7d7d4
Jose Fernandez <jrfo_04@unitec.edu>
2016-10-28 16:02:38 -0600

    avance

--- JoseFernandez-lab3.cpp
+++ JoseFernandez-lab3.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <math.h>	
+#include <string>
 using namespace std;
 
-int*** crearMatriz(int);
+//int*** crearMatriz(int);
+void imprimirMatriz(int, int**, int);
+void Permutaciones (int*);
+int Gregor();
+int** llenarMatriz(int**, int*, int);
 
 int  main(int argc, char const *argv[]) {
 	int opcion = 1;
@@ -16,20 +21,9 @@ int  main(int argc, char const *argv[]) {
 		cin >> opcion;
 		cout << endl;
 		if (opcion == 1) {
-			int bisiesto = 0;
-			for (int i = 0; i < 100; ++i) {
-				for (int j = 0; j < 12; ++j) {
-					if (j == 1) {
-						if (bisiesto == 4) {
-							bisiesto = 0;
-
-						}
-					} else {
-
-					}
-				}//Fin del año
-				bisiesto++;
-			}//Fin del siglo
+			int domingos = 0;
+			domingos = Gregor();
+			cout << domingos << endl;
 		}//Fin del if 1
 		if (opcion == 2) {
 			int grado = 0;
@@ -38,9 +32,9 @@ int  main(int argc, char const *argv[]) {
 			int polinomio[grado + 1];
 			int size = grado + 1;
 			int contador = 0;
-			for (int i = size; i >= 0; --i) {
+			for (int i = grado; i >= 0; --i) {
 				int valor = 0;
-				cout << "Ingrese el polinomio x^" << i;
+				cout << "Ingrese el polinomio x^" << i << ":";
 				cin >> valor;
 				polinomio[contador] = valor;
 				contador++; 
@@ -48,10 +42,22 @@ int  main(int argc, char const *argv[]) {
 			int a = 0;
 			cout << endl << "Ingrese a:";
 			cin >> a;
-			crearMatriz(size);
+			int** matriz = new int*[3];
+			for (int i = 0; i < 3; ++i) {
+				matriz[i] = new int[size];
+			}//Fin del for
+			matriz = llenarMatriz(matriz, polinomio, size);
+			imprimirMatriz(size, matriz, a);
 		}//Fin del if 2
 		if (opcion == 3) {
-
+			string numero = "";
+			cout << "Ingrese el numero:";
+			cin >> numero;
+			int numeros[4];
+			for (int i = 0; i < 4; ++i) {
+				numeros[i] = numero[i];
+			}//Fin del convertor
+			
 		}//Fin del if 3
 	} while (opcion != 4);
 	return 0;
@@ -59,18 +65,132 @@ int  main(int argc, char const *argv[]) {
 
 
 
+int Gregor () {
+	int dia = 1;
+	int final = 0;
+	for (int i = 1900; i < 2000; ++i) {
+		if (i%4 == 0) {
+			//BISIESTO
+			for (int j = 1; j <= 12; ++j) {
+				if (j == 9||j == 4||j == 6||j == 11) {
+					for (int k = 1; k <= 30; ++k) {
+						if (dia==7) {
+							if (k==1) {
+								final++;
+							}
+							dia = 1;
+						} else {
+							dia++;
+						}
+					}//fin
+				} else if (j == 2) {
+					for (int k = 1; k <= 29; ++k) {
+						if (dia==7) {
+							if (k==1) {
+								final++;
+							}
+							dia = 1;
+						} else {
+							dia++;
+						}	
+					}//Fin
+				} else {
+					for (int k = 1; k <= 31; ++k) {
+						if (dia==7) {
+							if (k==1) {
+								final++;
+							}
+							dia = 1;
+						} else {
+							dia++;
+						}
+					}//Fin
+				} 
+			}//Fin del año
+		} else {
+			//NO BISIESTO
+			for (int j = 1; j <= 12; ++j) {
+				if ((j == 9)||(j == 4)||(j == 6)||(j == 11)) {
+					for (int k = 1; k <= 30; ++k) {
+						if (dia==7) {
+							if (k==1) {
+								final++;
+							}
+							dia = 1;
+						} else {
+							dia++;
+						}
+					}//Fin
+				} else if (j == 2) {
+					for (int k = 1; k <= 28; ++k) {
+						if (dia==7) {
+							if (k==1) {
+								final++;
+							}
+							dia = 1;
+						} else {
+							dia++;
+						}		
+					}
+				} else {
+					for (int k = 1; k <= 31; ++k) {
+						if (dia==7) {
+							if (k==1) {
+								final++;
+							}
+							dia = 1;
+						} else {
+							dia++;
+						}		
+					}
+				} 
+			}//Fin del ano
+		}
+	}//Fin del siglo
+	return final+1;
+}//Fin del metodo
+
+int** llenarMatriz(int**matriz, int* arreglo, int size) {
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < size; ++j) {
+			if (i == 0) { 
+				matriz[0][j] = arreglo[j];
+			}
+		}
+	}
+	return matriz;
+}
 
 
+void imprimirMatriz(int size, int** matriz, int a) {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < size; j++) {
+			cout<< matriz[i][j] << " ";
+			if (j == size - 1 && i == 0) {
+				cout << "|" << a;
+			}
+		}
+		cout << endl;
+	}
+	cout << "--------------------" << endl;
+}
 
+void Permutaciones (int* arreglo) {
+
+}
+
+
+/*
 int*** crearMatriz(int size) {
 	int*** matriz = new int**[size];
-	for (int i = 0; i < size; ++i){
+	for (int i = 0; i < size; i++){
 		matriz[i] = new int*[3];
 	}
-	for (int i = 0; i < size; ++i) {
-		for (int j = 0; j < size; ++j) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < 3; j++) {
 			matriz[i][j] = new int[size];
 		}
 	}
 	return matriz;
 }//Fin de crear matriz
+*/
