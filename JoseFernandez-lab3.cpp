ec42f0793454fcc342b7fa6a368e2314b2e51b76
Jose Fernandez <jrfo_04@unitec.edu>
2016-10-28 14:10:12 -0600

    Se avanza con el segundo ejercicio

--- JoseFernandez-lab3.cpp
+++ JoseFernandez-lab3.cpp
@@ -14,11 +14,41 @@ int  main(int argc, char const *argv[]) {
 		cout << "4. Salir." << endl;
 		cout << "Ingrese su opcion:";
 		cin >> opcion;
+		cout << endl;
 		if (opcion == 1) {
+			int bisiesto = 0;
+			for (int i = 0; i < 100; ++i) {
+				for (int j = 0; j < 12; ++j) {
+					if (j == 1) {
+						if (bisiesto == 4) {
+							bisiesto = 0;
 
+						}
+					} else {
+
+					}
+				}//Fin del año
+				bisiesto++;
+			}//Fin del siglo
 		}//Fin del if 1
 		if (opcion == 2) {
-
+			int grado = 0;
+			cout << "Ingrese el grado mas alto del polinomio:";
+			cin >> grado;
+			int polinomio[grado + 1];
+			int size = grado + 1;
+			int contador = 0;
+			for (int i = size; i >= 0; --i) {
+				int valor = 0;
+				cout << "Ingrese el polinomio x^" << i;
+				cin >> valor;
+				polinomio[contador] = valor;
+				contador++; 
+			}
+			int a = 0;
+			cout << endl << "Ingrese a:";
+			cin >> a;
+			crearMatriz(size);
 		}//Fin del if 2
 		if (opcion == 3) {
 
@@ -26,6 +56,11 @@ int  main(int argc, char const *argv[]) {
 	} while (opcion != 4);
 	return 0;
 }//Fin del main
+
+
+
+
+
 
 int*** crearMatriz(int size) {
 	int*** matriz = new int**[size];
